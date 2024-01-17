970fe5efd4ddebb310de4a9f9a509c75e1b833ec
Franco Juri <francojuri22@gmail.com>
2024-01-17 18:33:42 -0300

    petition fixed

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -47,7 +47,7 @@ export const useAuthStore = () => {
         if(!token) return dispatch(onLogout());
 
         try {
-            const { data } = await calendarApi.get('auth/renew');
+            const { data } = await calendarApi.get('/auth/renew');
             localStorage.setItem('token', data.token);
             localStorage.setItem('token-init-date', new Date().getTime());
             dispatch(onLogin({ name: data.name, uid: data.uid }));
